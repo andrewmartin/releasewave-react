51b5d5091b62125781957183209d52d963a46640
Andrew Martin <hello@andrewmart.in>
2019-04-02 17:53:24 -0700

    update readme, lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,24 +13,9 @@ module.exports = {
   },
   rules: {
     'prefer-destructuring': ['error', { object: true, array: false }],
-    //   'object-property-newline': ['error'],
-    //   camelcase: 0,
-    //   'jsx-a11y/img-has-alt': 0,
-    //   'jsx-a11y/anchor-is-valid': 0,
-    //   'jsx-a11y/no-static-element-interactions': 0,
-    //   'jsx-a11y/click-events-have-key-events': 0,
-    //   'react/jsx-no-bind': 0,
     'react/prop-types': 0,
     'react/display-name': 0,
-    //   'react/prefer-stateless-function': 0,
-    //   'react/require-extension': 0,
-    //   'plugin:react/require-default-props': 0,
-    //   'react/forbid-prop-types': 0,
-    //   'no-param-reassign': ['error', { props: false }],
-    //   'no-multiple-empty-lines': [2, { max: 1, maxEOF: 1 }],
     'no-unused-vars': 2,
-    //   // 'max-len': ['error', { code: 80, ignoreUrls: true }],
-    //   'no-confusing-arrow': 'error',
     'prettier/prettier': [
       'error',
       {
@@ -41,9 +26,5 @@ module.exports = {
         jsxBracketSameLine: true,
       },
     ],
-    //   // TODO - Fix this so it will correctly follow webpack resolutions too
-    //   'import/no-unresolved': 0,
-    //   'import/no-extraneous-dependencies': 0,
-    //   'import/extensions': 0
   },
 };
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Release Wave React Frontend
 
+The frontend application for Release Wave, built using `Next.js` and React.
+
+## Quickstart
+
+- Copy `./env-example` to `./env`, and update proper variables
+- `yarn`
+- `yarn start`
+
+### Folder structure
+
 This project uses Next.js. [Original Next.js README here](doc/NEXT.md)
 
 ## Roadmap
@@ -17,6 +27,7 @@ This project uses Next.js. [Original Next.js README here](doc/NEXT.md)
   - [ ] Genres
 - [ ] User Index
 - [ ] User Profile
+- [ ] Tests
 
 ### Hotfixes
 
