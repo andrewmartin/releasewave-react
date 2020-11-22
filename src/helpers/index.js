d47965181c71b95b56e91b95f63c53324906f8df
Andrew Martin <hello@andrewmart.in>
2020-11-21 18:24:14 -0800

    log

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -8,6 +8,8 @@ export function capitalizeFirstLetter(string) {
 
 const currentDate = () => moment().clone();
 
+console.log('currentDate', currentDate());
+
 export const sixWeekWindow = [
   {
     name: moment(currentDate())
